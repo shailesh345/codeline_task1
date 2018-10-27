a438c9d02423ae607c452117a898819f36ebdbe8
shailesh <dwivedi.shailesh345@gmail.com>
2018-10-27 14:19:12 +0530

    films records data save on revision done

--- wp-content/themes/unite child/functions.php
+++ wp-content/themes/unite child/functions.php
@@ -5,7 +5,6 @@ function enqueue_child_theme_styles() {
 }
 
 ####### Add Type Post Films ########
-
 function codeline_films_post_type()
 {
 
@@ -35,10 +34,12 @@ $args=[
     'capability_type'=>'post',
     'hierarchical'=>FALSE,
     'supports'=>[
-        'title','editor','excerpt','thumbnail','revisions'
+        'title','editor','thumbnail','revisions'
     ],
     //'taxonomies'=>['category','post_tag'],
     'menu_position'=>2,
+    'menu_icon' => 'dashicons-editor-video',
+    'register_meta_box_cb' => 'codeline_add_films_metaboxes',
     'exclude_from_search'=>FALSE
 ];
 
@@ -86,4 +87,92 @@ register_taxonomy(strtolower($taxonomy),['films'], $args);
 
 add_action('init','codeline_custom_taxonomies');
 
+//Add custom text fields "Ticket Price" and "Release Date".
+
+//HTML data for Ticket Price 
+function codeline_films_ticketprice() {
+	global $post;
+	// Nonce field to validate form request came from current site
+	wp_nonce_field( basename( __FILE__ ), 'films_fields' );
+	// Get the ticket price data if it's already been entered
+	$ticketprice = get_post_meta( $post->ID, 'ticketprice', true );
+	// Output the field
+	echo '<input type="text" name="ticketprice" value="' . esc_textarea($ticketprice)  . '" class="widefat">';
+}
+
+function codeline_films_releasedate() {
+	global $post;
+	// Nonce field to validate form request came from current site
+	wp_nonce_field( basename( __FILE__ ), 'films_fields' );
+	// Get the ticket price data if it's already been entered
+	$releasedate = get_post_meta( $post->ID, 'releasedate', true );
+	// Output the field
+	echo '<input type="date" name="releasedate" value="' . esc_textarea($releasedate)  . '" class="widefat">';
+}
+
+function codeline_add_films_metaboxes() {
+//Ticket Price
+	add_meta_box(
+		'codeline_films_ticketprice',
+		'Ticket Price',
+		'codeline_films_ticketprice',
+		'films',
+		'normal',
+		'default'
+	);
+   
+   //Release Date
+   
+   	add_meta_box(
+		'codeline_films_releasedate',
+		'Release Date',
+		'codeline_films_releasedate',
+		'films',
+		'normal',
+		'default'
+	);
+   
+}
+add_action('add_meta_boxes','codeline_add_films_metaboxes');
+
+##### Save the metabox(Ticket Price and Release Date) data ########
+
+function codeline_save_films_meta($post_id,$post) {
+	// Return if the user doesn't have edit permissions.
+	if ( ! current_user_can( 'edit_post', $post_id ) ) {
+		return $post_id;
+	}
+	// Verify this came from the our screen and with proper authorization,
+	// because save_post can be triggered at other times.
+	if ( ! isset( $_POST['ticketprice'] ) || ! wp_verify_nonce( $_POST['films_fields'], basename(__FILE__) ) ) {
+		return $post_id;
+	}
+	// This sanitizes the data from the field and saves it into an array $films_meta.
+	$films_meta['ticketprice'] = esc_textarea( $_POST['ticketprice'] );
+   
+   	if ( ! isset( $_POST['releasedate'] ) || ! wp_verify_nonce( $_POST['films_fields'], basename(__FILE__) ) ) {
+		return $post_id;
+	}
+	// This sanitizes the data from the field and saves it into an array $films_meta.
+	$films_meta['releasedate'] = esc_textarea( $_POST['releasedate'] );
+   
+	foreach ( $films_meta as $key => $value ) :
+		// Don't store custom data twice
+		if ( 'revision' === $post->post_type ) {
+			return;
+		}
+		if ( get_post_meta( $post_id, $key, false ) ) {
+			// If the custom field already has a value, update it.
+			update_post_meta( $post_id, $key, $value );
+		} else {
+			// If the custom field doesn't have a value, add it.
+			add_post_meta( $post_id, $key, $value);
+		}
+		if ( ! $value ) {
+			// Delete the meta key if there's no value
+			delete_post_meta( $post_id, $key );
+		}
+	endforeach;
+}
+add_action('save_post','codeline_save_films_meta', 1, 2 );
 ?>
